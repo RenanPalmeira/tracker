03666ed1dd6bde946754c58c323a763983a94a5f
Renan Palmeira <renanpalmeira1@hotmail.com>
2017-08-07 02:23:02 -0300

    Initial Category model/pojo

---
+++ tracker/src/main/java/com/tracker/tracker/model/Category.java
@@ -0,0 +1,11 @@
+package com.tracker.tracker.model;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Data
+public class Category {
+    private String name;
+    private String color;
+}
